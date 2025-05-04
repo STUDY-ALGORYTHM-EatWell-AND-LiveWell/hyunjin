82c5f90e724420d9beab1e7cd3b7b9010eac436a
2hyunjinn <huenjin04@naver.com>
2025-05-04 16:51:00 +0900

    [7주차] 프로그래머스_1_개인정보수집유효시간_이현진

--- src/week_7/프로그래머스_1_개인정보수집유효시간_이현진.java
+++ src/week_7/프로그래머스_1_개인정보수집유효시간_이현진.java
@@ -13,43 +13,32 @@ public class 프로그래머스_1_개인정보수집유효시간_이현진 {
     }
 
     public static Map<String, Integer> term = new HashMap<>();
-    public static int[] todayN = new int[3];
 
     public static int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            todayN[i] = Integer.parseInt(today.split("\\.")[i]);
-        }
-
         for (String i : terms) {
             term.put(i.split(" ")[0], Integer.parseInt(i.split(" ")[1]));
         }
 
+        int day = Integer.parseInt(today.split("\\.")[0]) * 12 * 28
+                + Integer.parseInt(today.split("\\.")[1]) * 28
+                + Integer.parseInt(today.split("\\.")[2]);
+
         for (int i = 0; i < privacies.length; i++) {
-            String type = privacies[i].split(" ")[1];
-            String date = privacies[i].split(" ")[0];
-            int y = Integer.parseInt(date.split("\\.")[0]);
-            int m = Integer.parseInt(date.split("\\.")[1]);
-            int d = Integer.parseInt(date.split("\\.")[2]);
-
-            if (!calculateDate(y, m, d, type)) result.add(i + 1);
+            if (calDate(privacies[i]) <= day) {
+                result.add(i + 1);
+            }
         }
-
         return result.stream().mapToInt(i -> i).toArray();
     }
 
-    private static boolean calculateDate(int y, int m, int d, String type) {
-        int plusM = term.get(type);
-        m += plusM;
+    private static int calDate(String privacy) {
+        String type = privacy.split(" ")[1];
+        String y = privacy.split(" ")[0].split("\\.")[0];
+        String m = privacy.split(" ")[0].split("\\.")[1];
+        String d = privacy.split(" ")[0].split("\\.")[2];
 
-        if (m > 12) {
-            y = y + (m - 1) / 12;
-            m = (m - 1) % 12 + 1;
-        }
-
-        if (todayN[0] < y) return true;
-        else if (todayN[0] == y && todayN[1] < m) return true;
-        else if (todayN[0] == y && todayN[1] == m && todayN[2] < d) return true;
-        return false;
+        int day = Integer.parseInt(y) * 12 * 28 + Integer.parseInt(m) * 28 + Integer.parseInt(d);
+        return (day + term.get(type) * 28);
     }
 }
